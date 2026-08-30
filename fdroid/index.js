6f5742edd9d4a677b820e0b3f82ebe534b21e3dc
Jamie Achatz <jamieachatz@gmail.com>
2026-08-30 13:31:34 +0200

    c

---
+++ fdroid/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
+const app = express();
+
+// Tell Express that files ending in .html should be rendered with EJS's renderFile
+app.engine('html', ejs.renderFile);
+
+// Set the default view engine to .html so you can omit the extension in res.render()
+app.set('view engine', 'html');
+
+// Tell Express where your HTML templates live
+app.set('views', path.join(__dirname, 'views'));
+
+// Render the HTML from a route
+app.get('/', (req, res) => {
+    // Because we set the view engine to "html", we can just pass the name without the extension
+    res.render('index');   // Express will look for views/index.html
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
