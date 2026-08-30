8ea54fe6939295b14d4c571e1b8fe053223371ba
Jamie Achatz <jamieachatz@gmail.com>
2026-08-30 20:12:08 +0200

    c2

--- fdroid/index.js
+++ fdroid/index.js
@@ -1,11 +1,19 @@
-// fdroid/index.js
 const express = require('express');
 const path = require('path');
 const app = express();
 
+// Absolute Pfadbestimmung (kompatibel mit Serverless/Vercel)
 const repoPath = path.join(__dirname, 'fdroidrepo', 'repo');
 
-app.use('/', express.static(repoPath));
+// Statische Dateien direkt auf der Wurzel des Sub-Services anbieten
+app.use(express.static(repoPath));
+
+// Falls explizit /repo im Pfad mit übergeben wird
 app.use('/repo', express.static(repoPath));
+
+// Fallback für index.html auf der Startseite des Dienstes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(repoPath, 'index.html'));
+});
 
 module.exports = app;
