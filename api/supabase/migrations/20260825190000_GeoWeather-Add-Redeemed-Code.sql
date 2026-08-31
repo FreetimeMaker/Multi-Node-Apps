c87198b483c7e6bc3633d30ac345df8d01eb66bc
Jamie Achatz <jamieachatz@gmail.com>
2026-08-31 21:22:21 +0200

    Add API to own Repo

--- api/supabase/migrations/20260825190000_GeoWeather-Add-Redeemed-Code.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE public.geoweather_subscriptions
-ADD COLUMN IF NOT EXISTS redeemed_code_id UUID REFERENCES public.geoweather_codes(id) ON DELETE SET NULL;
--- api/supabase/migrations/20260827181500_Add-Category-To-Wallora.sql
+++
@@ -1,3 +0,0 @@
--- Add category column to wallora_wallpapers
-ALTER TABLE public.wallora_wallpapers
-ADD COLUMN category TEXT;
