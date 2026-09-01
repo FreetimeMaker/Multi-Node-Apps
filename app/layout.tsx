bad1f1e98e031f314674d88121c21b039b7df061
Jamie Achatz <jamieachatz@gmail.com>
2026-09-01 18:50:56 +0200

    c

--- app/layout.tsx
+++ app/layout.tsx
@@ -102,21 +102,6 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         <ThemeProvider>
           {children}
         </ThemeProvider>
-
-        {/* Google tag (gtag.js) */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-E05VRR273J"
-          strategy="afterInteractive"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-E05VRR273J');
-          `}
-        </Script>
       </body>
     </html>
   );
